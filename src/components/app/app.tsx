561558b1640021f366cd3f2e7ea51067a5c48705
Bilol <83568251+Bilol03@users.noreply.github.com>
2026-02-10 09:33:44 +0500

    Adding new movie part has been finished

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,7 @@
 import {AppFilter} from '../app-filter/app-filter'
 import { AppInfo } from '../app-info/app-info'
+import { MoviesAddForm } from '../movie-add-form/movie-add-form'
+import MovieList from '../movie-list/movie-list'
 import { SearchPanel } from '../search-panel/search-panel'
 
 import './app.css'
@@ -13,6 +15,8 @@ export function App() {
 					<SearchPanel />
 					<AppFilter />
 				</div>
+                <MovieList/>
+                <MoviesAddForm />
 			</div>
 		</div>
 	)
---
+++ src/components/movie-add-form/movie-add-form.tsx
@@ -0,0 +1,8 @@
+import './movie-add-form.css'
+
+export function MoviesAddForm() {
+	return <div className="movie-add-form">
+        <h1>Yangi kino qo'shish</h1>
+        <form action=""></form>
+    </div>
+}
