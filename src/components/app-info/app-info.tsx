8913b9de7b7f130ad2200b9780c7d0ae53573c72
Bilol <83568251+Bilol03@users.noreply.github.com>
2026-02-13 10:29:18 +0500

    movie list made dynamic

--- src/components/app-info/app-info.tsx
+++ src/components/app-info/app-info.tsx
@@ -1,6 +1,7 @@
 import './app-info.css'
  
 export const AppInfo = () => {
+    
 	return <div className='app-info'>
         <p className='fs-3 text-uppercase'>Umumiy kinolar soni: 0</p>
         <p className='fs-3 text-uppercase'>Ko'rilgan kinolar soni: 0</p>
