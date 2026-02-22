23e5aade97a5a5c39aa0e2533d5865b1e36bfe38
Bilol <83568251+Bilol03@users.noreply.github.com>
2026-02-22 15:09:30 +0500

    add movie inital steps

--- src/components/movie-add-form/movie-add-form.tsx
+++ src/components/movie-add-form/movie-add-form.tsx
@@ -2,41 +2,45 @@ import { Component } from 'react'
 import './movie-add-form.css'
 
 export class MoviesAddForm extends Component {
-    state: any
+	state: any
 	constructor(props: any) {
 		super(props)
-        this.state ={
-            name: '',
-            view: ''
-        }
+		this.state = {
+			name: '',
+			view: '',
+		}
 	}
 	onChangeInput = (e: any) => {
-        this.setState({
-            name: this.state.name +e.target.name.value,
-            view: this.state.view + e.target.view.value
-        })
-    }
-	onAddMovie = () =>  {
-        console.log(this.state.name)
-    }
+		this.setState({
+			[e.target.name]: e.target.value,
+			[e.target.view]: e.target.value,
+		})
+	}
+	onAddMovie = () => {
+		console.log(this.state.name)
+	}
 	render() {
+		const {name, view}:any = this.state
 		return (
 			<div className="movie-add-form">
 				<h3>Yangi kino qo'shish</h3>
 				<form className="add-form d-flex">
 					<input
 						type="text"
-                        name='name'
-                        onChange={this.onAddMovie}
+						name="name"
+						onChange={this.onAddMovie}
 						className="form-control new-post-label"
 						placeholder="Qaysi kino"
+						value={name}
 					/>
 					<input
 						type="number"
-                        name='view'
-                        onChange={this.onAddMovie}
+						name="view"
+						onChange={this.onAddMovie}
 						className="form-control new-post-label"
 						placeholder="Necha marotaba korilgan"
+						value={view}
+
 					/>
 					<button
 						type="submit"
